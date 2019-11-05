d80e833108c1d06079ce563c4c9b4d777487c0f7
Hữu Ðà Trần <huuda.tran@quoininc.com>
2019-11-05 16:45:25 -0500

    - npm outdated. Added nyc, jscpd, and github action

--- server/search/hit-mapper.js
+++ server/search/hit-mapper.js
@@ -4,8 +4,8 @@ const warpjsUtils = require('@warp-works/warpjs-utils');
 const CONTENT_LINK_RE = /{{(.*?),(.*?),(.*?)}}/g;
 
 function contentLinkReplacer(match, label, type, id) {
-    const href = RoutesInfo.expand('entity', {id, type});
-    const previewUrl = RoutesInfo.expand('W2:portal:preview', {id, type});
+    const href = RoutesInfo.expand('entity', { id, type });
+    const previewUrl = RoutesInfo.expand('W2:portal:preview', { id, type });
     return `<a href="${href}" data-warpjs-action="preview" data-warpjs-preview-url="${previewUrl}">${label}<span class="glyphicon glyphicon-link"></span></a>`;
 }
 
